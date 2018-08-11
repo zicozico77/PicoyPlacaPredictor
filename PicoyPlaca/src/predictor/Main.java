c93b93bfccf248f750332cafeb592aaf246e484e
diego robles <diego_robles99@hotmail.com>
2018-08-11 10:23:27 +1000

    Made main work with outside arguments

--- PicoyPlaca/src/predictor/Main.java
+++ PicoyPlaca/src/predictor/Main.java
@@ -3,9 +3,18 @@ package predictor;
 public class Main {
 
 	public static void main(String[] args) {
-		Predictor checker = new Predictor();
-		
-
+		int licensePlate = Integer.parseInt(args[0]);
+		String date = args[1];
+		Time time = new Time(Integer.parseInt(args[2]),
+					Integer.parseInt(args[3]),
+					Integer.parseInt(args[4]))
+		Predictor checker = new Predictor;
+		if(checker.check(licensePlate, date, time)) {
+			System.out.print("Car can transit safely");
+		} else {
+			System.out.print("Car can not transit currently due to Pico y Placa");
+		}
+				
 	}
 
 }
